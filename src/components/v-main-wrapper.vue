c0a85cc6d8e797621df3778828054c22d5d8dc06
Rustem Topuz <topuzr@mail.ru>
2024-04-19 19:42:22 +0300

    Дизайн

--- src/components/v-main-wrapper.vue
+++ src/components/v-main-wrapper.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="v-main-wrapper">
+    <h1 class="v-main-wrapper-title">CATALOG WITH LOVE <span class="heart">&#9829;</span></h1>
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
@@ -35,5 +36,12 @@ mounted(){}
 .v-main-wrapper{
     max-width: 1400px;
     margin: 0 auto;
+}
+.v-main-wrapper-title{
+  text-align: center;
+  color: black;
+}
+.heart{
+  color: red;
 }
 </style>
